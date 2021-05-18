72e7955a02c6a37cea145cc62f690c6af59e98a8
DivyankaChaudhari <divyanka.chaudhari25@gmail.com>
2021-05-17 20:07:19 -0400

    Added into folders, inputting map

---
+++ Subtask-1/header-files/menu.h
@@ -0,0 +1,14 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <unistd.h>
+#include "font.h"
+#include "constants.h"
+
+void server_or_client(SDL_Renderer *renderer, char *menu, TTF_Font *font);
+void ask_for_ip(SDL_Renderer *renderer, TTF_Font *font, char *ip);
+void ask_for_name(SDL_Renderer *renderer, TTF_Font *font, char *name);
+
+#endif
